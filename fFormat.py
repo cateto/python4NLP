cf6aa12548654eb0f9ee37910b6d8379294ff0d9
cateto <u2skind@gmail.com>
2021-05-11 15:20:11 +0900

    문자열 기초

---
+++ fFormat.py
@@ -0,0 +1,15 @@
+name = "김괜찮"
+age = "123234"
+
+print(f'저의 나이는 {name}이고 이름은 {age} 입니다.')
+
+dict = {'name' : '이잠수', 'age' : 144 }
+print(f'저의 이름은 {dict["name"]} 이고 나이는 {dict["age"]}입니다.')
+
+
+#과제 '!!!python!!!'출력하기
+
+print('{0:!^12}'.format('python'))
+
+print(f'{"python":!^12}')
+
--- strFormattion.py
+++ strFormattion.py
@@ -1,7 +1,23 @@
 a  = "I ate %d apples" % 3
-
 print(a)
 
 b = "I bought %s roses" % "hundred of"
-
 print(b)
+
+c = "I like {0} milk".format("strawberry")
+print(c)
+
+d = "Jenny got a {0} and I got a {1}".format("pony", "robot")
+print(d)
+
+e = "{0:>10}hello".format("hamburger")
+print(e)
+
+f = "{0:^10}hello".format("fries")
+print(f)
+
+g = "{0:~^10}".format("hi")
+print(g)
+
+h = "{0:!>10}".format("hi")
+print(h)
