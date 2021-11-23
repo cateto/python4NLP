ef3306333f92ac30e410eadb26a876590d67c5f7
cateto <u2skind@gmail.com>
2021-11-24 08:57:56 +0900

    faiss test

--- multi-processing/3.py
+++ multi-processing/3.py
@@ -1,9 +1,23 @@
 from multiprocessing import Process
 import time
 import os
+from functools import wraps
 
 start_time = time.time()
 
+
+def timefn(fn):
+    @wraps(fn)
+    def measure_time(*args, **kwargs):
+        t1 = time.time()
+        result = fn(*args, **kwargs)
+        t2 = time.time()
+        print(f"@timefn: {fn.__name__} took {t2 -t1} seconds")
+        return result
+    return measure_time
+
+
+@timefn
 def count(cnt):
     proc = os.getpid()
     for i in range(cnt):
